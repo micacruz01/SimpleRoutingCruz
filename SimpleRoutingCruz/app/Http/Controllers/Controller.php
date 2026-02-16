177fe72f79f516e2214935a0e0a6cf89d1f8a4d9
micacruz01 <micac0963@gmail.com>
2026-02-16 00:39:46 +0000

    simple routing updates

--- SimpleRoutingCruz/app/Http/Controllers/Controller.php
+++ SimpleRoutingCruz/app/Http/Controllers/Controller.php
@@ -1,11 +1,8 @@
+<?php
+
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
-class GreetController extends Controller
+abstract class Controller
 {
-    public function show()
-    {
-        return view('greet');
-    }
+    //
 }
